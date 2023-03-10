2db672c9efe6c2097bc9b0b2c4257ee1629a0614
Severin Kaderli <94976842+skaderli@users.noreply.github.com>
2023-03-10 15:01:27 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,19 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['numpy',
+                'xarray',
+                'netcdf4',
+                'pandas',
+                'scikit-image',
+                'scipy',
+                'itertools',
+                'tqdm'
+                'wrf-python','shapely',
+                'sklearn',
+                'matplotlib',
+                'cartopy',
+                'seaborn']
 
 test_requirements = ['pytest>=3', ]
 
