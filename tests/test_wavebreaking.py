4c4805a68f6e71d173d5f5351f38ddc28dc95dd8
Severin Kaderli <severin.kaderli@unibe.ch>
2023-04-06 11:18:03 +0200

    update tests

--- tests/test_wavebreaking.py
+++ tests/test_wavebreaking.py
@@ -12,6 +12,8 @@ import numpy as np
 import xarray as xr
 import pandas as pd
 
+import matplotlib.pyplot as plt
+
 import warnings
 import pytest
 
@@ -77,35 +79,76 @@ def test_set_up_automatic(wavebreakings):
     
 def test_calculate_momentum_flux(wavebreakings):
     wavebreakings.set_up()  
-    wavebreakings.calculate_momentum_flux('variable', 'variable')
+    wavebreakings.calculate_momentum_flux(variable_zonal = 'variable', variable_meridional = 'variable')
     assert "mflux" in wavebreakings.dataset.keys()
     assert type(wavebreakings.dataset.mflux) is xr.DataArray
 
 def test_calculate_smoothed_field(wavebreakings):
     wavebreakings.set_up() 
-    wavebreakings.calculate_smoothed_field('variable', 10)
+    wavebreakings.calculate_smoothed_field(variable = 'variable', passes = 10)
     assert "smooth_variable" in wavebreakings.dataset.keys()
     assert type(wavebreakings.dataset.smooth_variable) is xr.DataArray
     
 def test_get_contours(wavebreakings):
     wavebreakings.set_up()
-    wavebreakings.get_contours("variable", 2)
+    wavebreakings.get_contours(variable = "variable", level = 2)
     assert type(wavebreakings.contours) is pd.DataFrame
     assert list(wavebreakings.contours.columns) == ['date', 'coordinates', 'level', 'exp_lon', 'mean_lat']
     assert type(wavebreakings._contours_wb_calc) is pd.DataFrame
     
 def test_get_streamers(wavebreakings):
     wavebreakings.set_up()
-    wavebreakings.get_contours("variable", 2)
+    wavebreakings.get_contours(variable = "variable", level = 2)
     wavebreakings.get_streamers()
     assert type(wavebreakings.streamers) is pd.DataFrame
+    assert len(wavebreakings.streamers) == 15
     assert list(wavebreakings.streamers.columns) == ['date', 'coordinates', 'mean_var', 'area', 'com']
     
 def test_get_overturnings(wavebreakings):
     wavebreakings.set_up()
-    wavebreakings.get_contours("variable", 2)
+    wavebreakings.get_contours(variable = "variable", level = 2)
     wavebreakings.get_overturnings()
     assert type(wavebreakings.overturnings) is pd.DataFrame
+    assert len(wavebreakings.overturnings) == 12
     assert wavebreakings.overturnings.columns.tolist() == ['date', 'coordinates', 'mean_var', 'area', 'com']
     
+def test_to_xarray(wavebreakings):
+    wavebreakings.set_up()
+    wavebreakings.get_contours(variable = "variable", level = 2)
+    wavebreakings.get_overturnings()
+    wavebreakings.to_xarray(events = wavebreakings.overturnings)
+    assert "flag" in wavebreakings.dataset.keys()
+    assert type(wavebreakings.dataset.flag) is xr.DataArray
     
+def test_event_tracking(wavebreakings):
+    wavebreakings.set_up()
+    wavebreakings.get_contours(variable = "variable", level = 2)
+    wavebreakings.get_overturnings()
+    wavebreakings.event_tracking(events = wavebreakings.overturnings)
+    assert type(wavebreakings.labeled_events) is pd.DataFrame
+    assert len(set(wavebreakings.labeled_events.label)) == 7
+    assert wavebreakings.labeled_events.columns.tolist() == ['date', 'coordinates', 'mean_var', 'area', 'com', 'label']
+    
+def test_plot_clim(wavebreakings):
+    try:
+        wavebreakings.set_up()
+        wavebreakings.plot_clim(variable = "variable")
+    except:
+        assert False
+        
+def test_plot_step(wavebreakings):
+    try:
+        wavebreakings.set_up()
+        wavebreakings.plot_step(flag_variable = "variable", variable = "variable", contour_level = [2], step = 0)
+    except:
+        assert False    
+
+def test_plot_tracks(wavebreakings):
+    #try:
+    wavebreakings.set_up()
+    wavebreakings.get_contours(variable = "variable", level = 2)
+    wavebreakings.get_overturnings()
+    wavebreakings.event_tracking(events = wavebreakings.overturnings)
+    wavebreakings.plot_tracks(events = wavebreakings.labeled_events)
+    #except:
+    #    assert False    
