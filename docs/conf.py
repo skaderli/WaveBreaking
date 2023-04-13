2928dd5e26e3d8a2e52f7f437b99a602fce6ea46
Severin Kaderli <94976842+skaderli@users.noreply.github.com>
2023-04-13 10:46:32 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-autodoc_mock_imports = ["numpy"]
-
 import wavebreaking
 
 # -- General configuration ---------------------------------------------
