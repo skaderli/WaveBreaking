1d46d2617a6687072b7cab8bacf8ea33aae1a517
Severin Kaderli <severin.kaderli@unibe.ch>
2023-05-08 14:10:24 +0200

    update description

--- wavebreaking/__init__.py
+++ wavebreaking/__init__.py
@@ -13,7 +13,7 @@ Top-level package for WaveBreaking.
 __author__ = "Severin Kaderli"
 __license__ = "MIT"
 __email__ = "severin.kaderli@unibe.ch"
-__version__ = "0.3.0"
+__version__ = '0.3.0'
 
 # import spatial pre-processing functions
 from wavebreaking.processing.spatial import (
