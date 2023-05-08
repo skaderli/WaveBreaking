f6d5551777a2d529e8d8d7f2f028ce9173a5462b
Severin Kaderli <severin.kaderli@unibe.ch>
2023-05-08 10:38:50 +0200

    update description

--- tests/test_wavebreaking.py
+++ tests/test_wavebreaking.py
@@ -7,7 +7,7 @@ The tool was developed during my master thesis at the University of Bern.
 Link to thesis: https://occrdata.unibe.ch/students/theses/msc/406.pdf
 
 ---
-Test for WaveBreaking
+Test classes for WaveBreaking
 """
 
 __author__ = "Severin Kaderli"
