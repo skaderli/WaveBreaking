4e7851264cb985fe7ac28c04ca2c7021b5b17cf6
Severin Kaderli <severin.kaderli@unibe.ch>
2023-06-06 07:00:28 +0200

    update prop calculation and coords transformation

--- wavebreaking/utils/index_utils.py
+++ wavebreaking/utils/index_utils.py
@@ -22,7 +22,6 @@ import geopandas as gpd
 from shapely.geometry import Polygon, MultiPolygon
 from shapely.ops import linemerge, unary_union, polygonize
 from shapely.validation import make_valid
-import shapely.vectorized
 from tqdm import tqdm
 import functools
 
@@ -33,97 +32,154 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 
 
-def properties_per_event(data, series, intensity, periodic_add, *args, **kwargs):
-    def polygon_to_grid_points(poly):
-        """
-        Extract all grid cells that are enclosed by the path of a polygon
-        """
-        # get grid points
-        x, y = np.meshgrid(
-            np.arange(0, kwargs["nlon"] + int(periodic_add / kwargs["dlon"])),
-            np.arange(0, kwargs["nlat"]),
-        )
-        x, y = x.flatten(), y.flatten()
-
-        # get interior points and border
-        mask_contain = shapely.vectorized.contains(poly.buffer(0.5), x, y)
-
-        return pd.DataFrame(np.c_[x, y][mask_contain], columns=["x", "y"]).astype("int")
-
-    # get grid points
-    points = polygon_to_grid_points(series.geometry)
-
-    # select date and coordinates
-    da = data.sel({kwargs["time_name"]: series.date}).values
-    lons = data[kwargs["lon_name"]].values
-    lats = data[kwargs["lat_name"]].values
-
-    # Calculate cell weights following the definition by Daniel Steinfeld
-    weight_lat = np.cos(data[kwargs["lat_name"]].values * np.pi / 180)
-    weight = (
-        np.ones((kwargs["nlat"], kwargs["nlon"]))
-        * np.array((111 * 111 * weight_lat))[:, None]
+def calculate_properties(events, data, intensity, periodic_add, **kwargs):
+    # get all grid points
+    x, y = np.meshgrid(
+        np.arange(0, kwargs["nlon"] + int(periodic_add / kwargs["dlon"])),
+        np.arange(0, kwargs["nlat"]),
+    )
+    x, y = x.flatten(), y.flatten()
+    points = gpd.GeoDataFrame(
+        pd.DataFrame({"x": x, "y": y}), geometry=gpd.points_from_xy(x, y)
     )
 
-    # calculate properties
-    areas = weight[points.y, points.x % kwargs["nlon"]]
-    area = np.sum(areas)
-    mean_var = np.dot(da[points.y, points.x % kwargs["nlon"]], areas) / area
+    # get coordinates of all events
+    buffer = events.copy()
+    buffer.geometry = buffer.geometry.buffer(
+        ((kwargs["dlon"] + kwargs["dlat"]) / 2) / 2
+    )
+    merged = gpd.sjoin(buffer, points, how="inner", predicate="contains").sort_index()
 
-    com = np.sum(points.multiply(areas, axis=0), axis=0) / area
-    com = [(lons[int(com.x) % kwargs["nlon"]], lats[int(com.y)])]
+    # get original coordinates
+    merged["lon"] = data[kwargs["lon_name"]].values[merged.x % kwargs["nlon"]]
+    merged["lat"] = data[kwargs["lat_name"]].values[merged.y]
 
-    # transform coordinates
+    # calculate area equator in km^2
+    area_cell = (
+        np.round(6371 * 2 * np.pi / (360 / ((kwargs["dlon"] + kwargs["dlat"]) / 2)))
+        ** 2
+    )
+    weight_lat = np.cos(data[kwargs["lat_name"]].values * np.pi / 180) * area_cell
+    merged["areas"] = weight_lat[merged.y]
+
+    # calculate mean_var, intensity and centre of mass
+    merged["mean_var"] = (
+        merged.areas
+        * data.loc[
+            {
+                kwargs["time_name"]: merged.date.to_xarray(),
+                kwargs["lat_name"]: merged.lat.to_xarray(),
+                kwargs["lon_name"]: merged.lon.to_xarray(),
+            }
+        ]
+    )
+
+    if intensity is not None:
+        merged["intensity"] = (
+            merged.areas
+            * intensity.loc[
+                {
+                    kwargs["time_name"]: merged.date.to_xarray(),
+                    kwargs["lat_name"]: merged.lat.to_xarray(),
+                    kwargs["lon_name"]: merged.lon.to_xarray(),
+                }
+            ]
+        )
+    else:
+        merged["intensity"] = 0
+
+    merged["x_com"] = merged.x * merged.areas
+    merged["y_com"] = merged.y * merged.areas
+
+    agg_merged = merged.groupby("id").agg(
+        {
+            "areas": "sum",
+            "mean_var": "sum",
+            "intensity": "sum",
+            "x_com": "sum",
+            "y_com": "sum",
+        }
+    )
+
+    # calculate centre of mass
+    com_x = data[kwargs["lon_name"]].values[
+        (agg_merged.x_com / agg_merged.areas).astype("int") % kwargs["nlon"]
+    ]
+    com_y = data[kwargs["lat_name"]].values[
+        (agg_merged.y_com / agg_merged.areas).astype("int")
+    ]
+    com = list(map(tuple, np.c_[com_x, com_y]))
+
+    prop_dict = {
+        "date": events.date,
+        "level": events.level,
+        "com": com,
+        "mean_var": (agg_merged.mean_var / agg_merged.areas).round(2),
+        "intensity": (agg_merged.intensity / agg_merged.areas).round(2),
+        "area": agg_merged.areas.round(2),
+    }
+
+    # add orientation if available
+    if "orientation" in events.columns:
+        prop_dict["orientation"] = events.orientation
+
+    return pd.DataFrame(prop_dict)
+
+
+def transform_polygons(events, data, **kwargs):
     def transform_coords(polygon):
         """
         Transform coordinates to original grid
         """
+        # get coordinates and check split and last meridian
         coords = np.asarray(polygon.exterior.coords.xy).T.astype("int")
-        coords = np.c_[coords[:, 0] % kwargs["nlon"], coords[:, 1]]
-        return Polygon(np.c_[lons[coords[:, 0]], lats[coords[:, 1]]])
+        split = (coords >= kwargs["nlon"]).any()
 
-    # check if polygon needs to be split at last meridian
-    if any(points.x >= kwargs["nlon"]):
-        # split polygons at last meridian
+        # transform coordinates
+        coords = np.c_[coords[:, 0] % kwargs["nlon"], coords[:, 1]]
+
+        return split, Polygon(
+            np.c_[
+                data[kwargs["lon_name"]][coords[:, 0]],
+                data[kwargs["lat_name"]][coords[:, 1]],
+            ]
+        )
+
+    def split_polys(polygon):
+        """
+        Split polygons at the last meridian
+        """
+        # define last meridian
         p00, p01 = [kwargs["nlon"] - 1, kwargs["nlat"]], [kwargs["nlon"] - 1, 0]
         p10, p11 = [kwargs["nlon"], 0], [kwargs["nlon"], kwargs["nlat"]]
         meridian = Polygon([p00, p01, p10, p11])
-        merged = linemerge([series.geometry.boundary, meridian.boundary])
+
+        # split polygons
+        merged = linemerge([polygon.boundary, meridian.boundary])
         borders = unary_union(merged)
         polygons = [
             p
             for p in polygonize(borders)
-            if not meridian.contains(p) and make_valid(series.geometry).contains(p)
+            if not meridian.contains(p) and make_valid(polygon).contains(p)
         ]
 
+        # transform if possible
         if len(polygons) == 0:
-            return gpd.GeoDataFrame()
+            return Polygon()
         else:
-            polys = [transform_coords(p) for p in polygons]
-            poly = MultiPolygon(polys)
-    else:
-        poly = transform_coords(series.geometry)
-
-    # store properties in dict
-    prop_dict = {
-        "date": series.date,
-        "level": series.level,
-        "com": com,
-        "mean_var": np.round(mean_var, 2),
-        "area": np.round(area, 2),
-    }
-
-    # calculate intensity if provided
-    if intensity is not None:
-        intens = intensity.sel({kwargs["time_name"]: series.date}).values
-        intens = np.dot(intens[points.y, points.x % kwargs["nlon"]], areas) / area
-        prop_dict["intensity"] = np.round(intens, 2)
+            polys = [transform_coords(p)[1] for p in polygons]
+            return MultiPolygon(polys)
 
     # return GeoDataFrame
-    return gpd.GeoDataFrame(
-        pd.DataFrame(prop_dict),
-        geometry=[poly],
+    gdf = gpd.GeoDataFrame(
+        [transform_coords(row.geometry) for index, row in events.iterrows()],
+        columns=["split", "geometry"],
     )
+    gdf.loc[gdf.split, "geometry"] = [
+        split_polys(row.geometry) for index, row in events[gdf.split].iterrows()
+    ]
+
+    return gdf
 
 
 def combine_shared(lst):
