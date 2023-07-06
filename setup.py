e96ff9605f542bacce92355aa71e52f3bafebbb2
Severin Kaderli <severin.kaderli@unibe.ch>
2023-07-06 17:37:11 +0200

    add python as requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy',
+requirements = ['python >= 3.9',
+                'numpy',
                 'xarray',
                 'netcdf4',
                 'pandas',
