acff28017e690a6024daf0e22f407fcb10ad8717
Severin Kaderli <severin.kaderli@unibe.ch>
2023-07-07 08:32:08 +0200

    update version number

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/skaderli/wavebreaking',
-    version="0.3.6",
+    version="0.3.7",
     zip_safe=False,
 )
